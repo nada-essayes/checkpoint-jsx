33cc0e1c51569a15d5578df2979a5580dd1ed779
nada-essayes <nadaessayes2017@gmail.com>
2021-12-31 14:22:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="style" >
+
+      <h1  className="title red">Your name here</h1>
+      
+      <br ></br>
+      <img src="/imageInSrc.jpg"  ></img>
+      <br ></br>
+      <img src="/imageInPublic.jpg" />
+    </div>
+    <video width="320" height="240" controls>
+      <source src="myVideo.mp4" type="video/mp4"  />
+    </video>
     </div>
   );
 }
